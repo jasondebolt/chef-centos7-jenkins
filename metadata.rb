2a0e62238327186d7406ef346ddfcbeeb82be349
Jason DeBolt <jasondebolt@gmail.com>
2017-06-04 14:45:40 -0700

    updating jenkins recipe

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,14 @@ maintainer_email 'jasondebolt@gmail.com'
 license 'all_rights'
 description 'Installs/Configures chef-centos7-jenkins'
 long_description 'Installs/Configures chef-centos7-jenkins'
-version '0.1.8'
+version '0.2.4'
 
 issues_url 'https://github.com/jasondebolt/chef-centos7-jenkins/issues' if respond_to?(:issues_url)
 
 source_url 'https://github.com/jasondebolt/chef-centos7-jenkins' if respond_to?(:source_url)
 
 depends 'chef-centos7-common'
+depends 'chef-centos7-workstation'
 depends 'chef-centos7-httpd'
 depends 'java'
 depends 'jenkins'
